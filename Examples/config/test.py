f63c4f409393eaaf18ca8f781fe3cc7094af0eca
russianidiot <russianidiot.github@gmail.com>
2016-06-08 00:26:55 +0300

    ^.coveragerc,.setup/__init__.py,.setup/pypi.python.org/open.command,.setup/ ...

--- Examples/config/test.py
+++ Examples/config/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-from os.path import dirname, exists, join
+import os
 from gitconfig import GitConfig
 
-root = dirname(dirname(dirname(__file__)))
-path = join(root, ".git", "config")
+root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+path = os.path.join(root, ".git", "config")
 config = GitConfig(path)
 print(config.sections)
